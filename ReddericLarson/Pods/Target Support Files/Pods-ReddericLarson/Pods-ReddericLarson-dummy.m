ba63ba06d4e045fb5f180e436d8ef654273b36f5
DNguyen17 <danhn@smu.edu>
2016-09-06 10:37:24 -0500

    flickr api up

---
+++ ReddericLarson/Pods/Target Support Files/Pods-ReddericLarson/Pods-ReddericLarson-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReddericLarson : NSObject
+@end
+@implementation PodsDummy_Pods_ReddericLarson
+@end
--- ReddericLarson/ReddericLarson/ViewController.m
+++ ReddericLarson/ReddericLarson/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "FlickrKit/FlickrKit.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,48 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [[FlickrKit sharedFlickrKit] initializeWithAPIKey:@"9e4dfb22612734eb30eefba263607c44" sharedSecret:@"df674246cac5a293"];
+    
+    FlickrKit *fk = [FlickrKit sharedFlickrKit];
+    FKFlickrInterestingnessGetList *interesting = [[FKFlickrInterestingnessGetList alloc] init];
+    [fk call:interesting completion:^(NSDictionary *response, NSError *error) {
+        // Note this is not the main thread!
+        if (response) {
+            NSMutableArray *photoURLs = [NSMutableArray array];
+            for (NSDictionary *photoData in [response valueForKeyPath:@"photos.photo"]) {
+                NSURL *url = [fk photoURLForSize:FKPhotoSizeSmall240 fromPhotoDictionary:photoData];
+                [photoURLs addObject:url];
+                NSLog(@"url = %@",url);
+            }
+            dispatch_async(dispatch_get_main_queue(), ^{
+                // Any GUI related operations here
+            });
+        }   
+    }];
+    
+    [[FlickrKit sharedFlickrKit] call:@"flickr.photos.search" args:@{@"text": @"cat", @"per_page": @"15"} maxCacheAge:FKDUMaxAgeOneHour completion:^(NSDictionary *response, NSError *error) {
+        NSMutableArray *photoURLs = [NSMutableArray array];
+        if (response) {
+            for (NSDictionary *photoData in [response valueForKeyPath:@"photos.photo"]) {
+                NSURL *url = [fk photoURLForSize:FKPhotoSizeSmall240 fromPhotoDictionary:photoData];
+                [photoURLs addObject:url];
+                NSLog(@"cat_url = %@",url);
+            }
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if (response) {
+                    // extract images from the response dictionary
+                    
+                } else {
+                    // show the error
+                }
+            });
+        }
+        else {
+            NSLog(@"Failed");
+        }
+    }];
+    
+
 }
 
 - (void)didReceiveMemoryWarning {
