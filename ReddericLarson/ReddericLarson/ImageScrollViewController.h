17de6483e57a52056a810d1030b8e249bae8bca8
DNguyen17 <danhn@smu.edu>
2016-09-09 13:41:49 -0500

    generic scrollview up

--- ReddericLarson/ReddericLarson/ImageScrollViewController.h
+++ ReddericLarson/ReddericLarson/ImageScrollViewController.h
@@ -7,7 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <FlickrKit/FlickrKit.h>
+#import "ImageModel.h"
 
 @interface ImageScrollViewController : UIViewController
-
+//@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (strong, nonatomic) UIImageView* imageView;
 @end
