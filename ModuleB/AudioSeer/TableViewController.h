457f80f6ae6bef6e32f6ab178b5c65670f67e040
egabrielsen <egabrielsen@smu.edu>
2016-09-19 10:33:41 -0500

    Module B init

---
+++ ModuleB/AudioSeer/TableViewController.h
@@ -0,0 +1,16 @@
+//
+//  TableViewController.h
+//  AudioLab
+//
+//  Created by Danh Nguyen on 9/15/16.
+//  Copyright © 2016 Eric Larson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ViewController.h"
+
+@interface TableViewController : UITableViewController
+
+@property (nonatomic,strong) ViewController* graphViewController;
+
+@end
