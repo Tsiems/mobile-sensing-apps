b2479eee10a83e3b27f75cee37db82692903f95f
egabrielsen <egabrielsen@smu.edu>
2016-09-27 12:14:20 -0500

    goal setting

--- GetFit/GetFit/ViewController.swift
+++ GetFit/GetFit/ViewController.swift
@@ -8,18 +8,86 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var goalLabel: UILabel!
+    @IBOutlet weak var newGoalField: UITextField!
+    
+    let numberToolbar: UIToolbar = UIToolbar()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if UserDefaults.standard.object(forKey: "stepGoal") == nil {
+            UserDefaults.standard.set(100, forKey: "stepGoal")
+            print("set goal 100")
+            goalLabel.text = "Step Goal: \(100)"
+        } else {
+            let number = UserDefaults.standard.integer(forKey: "stepGoal")
+            goalLabel.text = "Step Goal: \(number)"
+        }
+        
+        self.newGoalField.delegate = self
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
+        numberToolbar.barStyle = UIBarStyle.black
+        numberToolbar.tintColor = UIColor.white
+        numberToolbar.items=[
+            UIBarButtonItem(title: "Cancel", style: UIBarButtonItemStyle.plain, target: self, action: #selector(ViewController.dismissKeyboard)),
+            UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: self, action: nil),
+            UIBarButtonItem(title: "Apply", style: UIBarButtonItemStyle.plain, target: self, action: #selector(ViewController.returnSetGoal))
+        ]
+        
+        numberToolbar.sizeToFit()
+        
+        numberToolbar.sizeToFit()
+        
+        self.newGoalField.inputAccessoryView = numberToolbar
+    }
+    
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
+    func returnSetGoal() {
+        setGoal(self)
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if textField == self.newGoalField {
+            setGoal(self)
+            self.view.endEditing(true)
+        }
+        
+        
+        
+        return true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        
+    }
 
+    @IBAction func setGoal(_ sender: AnyObject) {
+        let goal = self.newGoalField.text!
+        let goalNumber = Int(goal)
+        UserDefaults.standard.set(goalNumber!, forKey: "stepGoal")
+        goalLabel.text = "Step Goal: \(goalNumber!)"
+
+    }
 
 }
 
