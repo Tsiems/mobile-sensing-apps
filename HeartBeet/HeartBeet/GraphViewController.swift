47d3b37a4dccdc37071e0c30a5ee6b7ef86a0596
derivativetaste <cmatthie@smu.edu>
2016-10-13 21:14:19 -0500

    clear background, color edits, nonfunctional minimal

--- HeartBeet/HeartBeet/GraphViewController.swift
+++ HeartBeet/HeartBeet/GraphViewController.swift
@@ -30,6 +30,7 @@ class GraphViewController: GLKViewController {
     
     func update() {
         let buffer = self.bridge.getRed()
+        
 //        let data = UnsafeMutablePointer<Float>.allocate(capacity: MemoryLayout<Float>.size*200)
 //        buffer?.fetchFreshData(data, withNumSamples: 200)
         self.graphHelper.setGraphData(buffer, withDataLength: 200, forGraphIndex: 0, withNormalization: 50, withZeroValue: 250)
