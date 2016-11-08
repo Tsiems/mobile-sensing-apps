a75828682d770acc73a75a2c7d0de5f7224cbd4d
DNguyen17 <danhn@smu.edu>
2016-11-07 21:47:46 -0600

    sprite kit set up

---
+++ AirDrummer/AirDrummer/SpriteScene.swift
@@ -0,0 +1,15 @@
+//
+//  SpriteScene.swift
+//  AirDrummer
+//
+//  Created by Danh Nguyen on 11/7/16.
+//  Copyright © 2016 Danh Nguyen. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class SpriteScene: SKScene {
+    
+
+}
