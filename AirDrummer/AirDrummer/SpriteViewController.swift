59fbe30ce4e935b8b60d4aade605fa458f274404
DNguyen17 <danhn@smu.edu>
2016-11-07 21:35:09 -0600

    storyboard

---
+++ AirDrummer/AirDrummer/SpriteViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SpriteViewController.swift
+//  AirDrummer
+//
+//  Created by Danh Nguyen on 11/7/16.
+//  Copyright © 2016 Danh Nguyen. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class SpriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
