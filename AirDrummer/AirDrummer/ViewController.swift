28291459ade5d7899d71b9c070445515d80d3526
DNguyen17 <danhn@smu.edu>
2016-11-08 20:37:28 -0600

    coremotion data, need url session stuff

--- AirDrummer/AirDrummer/ViewController.swift
+++ AirDrummer/AirDrummer/ViewController.swift
@@ -9,18 +9,61 @@
 import UIKit
 import CoreMotion
 
-class ViewController: UIViewController {
+let SERVER_URL = "http://10.8.110.111:8000"
+let UPDATE_INTERVAL = 1/10.0
 
+class ViewController: UIViewController, URLSessionTaskDelegate {
+
+    @IBOutlet weak var instrumentLabel: UILabel!
+    
+    var session = URLSession()
+    let cmMotionManager = CMMotionManager()
+    let backQueue = OperationQueue()
+    let ringBuffer = RingBuffer()
+    let magValue = 0.1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // setup URLSession 
+        let sessionConfig = URLSessionConfiguration.ephemeral
+        sessionConfig.timeoutIntervalForRequest = 5.0
+        sessionConfig.timeoutIntervalForResource = 8.0
+        sessionConfig.httpMaximumConnectionsPerHost = 1
+        
+        self.session = URLSession(configuration: sessionConfig, delegate: self, delegateQueue: nil)
+        self.startCMMonitoring()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func prepareForSample(instrumentName:String) {
+        self.instrumentLabel.text = "Play \(instrumentName)"
+    }
 
+    
+    func handleMotion(motion:CMDeviceMotion?, error:Error?)->Void{
+        self.ringBuffer.addNewData(Float((motion?.userAcceleration.x)!), withY: Float((motion?.userAcceleration.y)!), withZ: Float((motion?.userAcceleration.z)!))
+        let mag = fabs((motion?.userAcceleration.x)!)+fabs((motion?.userAcceleration.y)!)+fabs((motion?.userAcceleration.z)!)
+        if(mag > self.magValue) {
+            print(mag)
+        }
+    }
+    
+    func startCMMonitoring(){
+        if self.cmMotionManager.isDeviceMotionAvailable {
+            // update from this queue
+            self.cmMotionManager.deviceMotionUpdateInterval = UPDATE_INTERVAL
+            self.cmMotionManager.startDeviceMotionUpdates(to: backQueue, withHandler:self.handleMotion)
+        }
+    }
+    
 
+    
 }
 
