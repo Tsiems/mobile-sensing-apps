0c935ea500b372d79ac6e660f106790f10daaa0c
Tsiems <tsiems@smu.edu>
2016-12-07 15:51:17 -0600

    updated play view controller

--- AirDrummer/AirDrummer/PlayTableViewController.swift
+++ AirDrummer/AirDrummer/PlayTableViewController.swift
@@ -20,70 +20,186 @@ class PlayTableViewController: UITableViewController, URLSessionTaskDelegate {
     let magValue = 1.0
     var numDataPoints = 0
     
-    var hihatPlayer = AVAudioPlayer()
-    var snarePlayer = AVAudioPlayer()
-    var kickdrumPlayer = AVAudioPlayer()
-
-
+    let TIME_DELAY = 0.2
+    
+    
+    
+    //    var players = [String: [String: Any]]()
+    //    players["Hi-Hat"] = ["index":0,"players":[AVAudioPlayer(),AVAudioPlayer(),AVAudioPlayer()]
+    //
+    var players = [
+        "Hi-Hat":["filename":"hihat","index":0,"players":[AVAudioPlayer(),AVAudioPlayer(),AVAudioPlayer()]],
+        "Snare":["filename":"snare","index":0,"players":[AVAudioPlayer(),AVAudioPlayer(),AVAudioPlayer()]],
+        "Cymbal":["filename":"crash","index":0,"players":[AVAudioPlayer(),AVAudioPlayer(),AVAudioPlayer()]],
+        "Toms":["filename":"tom_002b","index":0,"players":[AVAudioPlayer(),AVAudioPlayer(),AVAudioPlayer()]],
+        "Bass":["filename":"bassdrum","index":0,"players":[AVAudioPlayer(),AVAudioPlayer(),AVAudioPlayer()]]
+    ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
         
+        players["Hi-Hat"]!["time"] = Date()
+        players["Snare"]!["time"] = Date()
+        players["Cymbal"]!["time"] = Date()
+        players["Toms"]!["time"] = Date()
+        players["Bass"]!["time"] = Date()
+        
+        
+        // Do any additional setup after loading the view.
+        // setup URLSession
         let sessionConfig = URLSessionConfiguration.ephemeral
         sessionConfig.timeoutIntervalForRequest = 5.0
         sessionConfig.timeoutIntervalForResource = 8.0
         sessionConfig.httpMaximumConnectionsPerHost = 1
         
-        // set up sound
-        let hihatSound = NSURL(fileURLWithPath: Bundle.main.path(forResource: "hihat", ofType: "wav")!)
         
-        do {
-            self.hihatPlayer = try AVAudioPlayer(contentsOf: hihatSound as URL)
-            self.hihatPlayer.prepareToPlay()
+        // set up sounds
+        for key in self.players.keys {
+            
+            let dict = self.players[key]!
+            
+            let sound = NSURL(fileURLWithPath: Bundle.main.path(forResource: dict["filename"] as! String?, ofType: "wav")!)
+            
+            do {
+                if var playersArray = dict["players"]! as? Array<AVAudioPlayer> {
+                    
+                    var i:Int = 0
+                    while i < (playersArray.count as Int)  {
+                        playersArray[i] = try AVAudioPlayer(contentsOf: sound as URL)
+                        playersArray[i].prepareToPlay()
+                        i += 1
+                    }
+                    
+                    self.players[key]!["players"] = playersArray
+                }
+            }
+            catch {
+                //
+            }
         }
-        catch {
-            //
-        }
-        
-        let kickdrumSound = NSURL(fileURLWithPath: Bundle.main.path(forResource: "kickdrum", ofType: "wav")!)
-        
-        do {
-            self.kickdrumPlayer = try AVAudioPlayer(contentsOf: kickdrumSound as URL)
-            self.kickdrumPlayer.prepareToPlay()
-        }
-        catch {
-            //
-        }
-        
-        
-        let snareSound = NSURL(fileURLWithPath: Bundle.main.path(forResource: "snare", ofType: "wav")!)
-        
-        do {
-            self.snarePlayer = try AVAudioPlayer(contentsOf: snareSound as URL)
-            self.snarePlayer.prepareToPlay()
-        }
-        catch {
-            //
-        }
-        
-        
-        //        self.hihatPlayer.play()
-        //        self.snarePlayer.play()
-        //        self.kickdrumPlayer.play()
         
         self.session = URLSession(configuration: sessionConfig, delegate: self, delegateQueue: nil)
         self.startCMMonitoring()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.cmMotionManager.stopDeviceMotionUpdates()
+    }
+    
+    
+    func handleMotion(motion:CMDeviceMotion?, error:Error?)->Void{
+        self.ringBuffer.addNewData(Float((motion?.userAcceleration.x)!), withY: Float((motion?.userAcceleration.y)!), withZ: Float((motion?.userAcceleration.z)!))
+        
+        self.orientationBuffer.addNewData(Float((motion?.attitude.pitch)!), withY: Float((motion?.attitude.roll)!), withZ: Float((motion?.attitude.yaw)!))
+        
+        let mag = fabs((motion?.userAcceleration.x)!)+fabs((motion?.userAcceleration.y)!)+fabs((motion?.userAcceleration.z)!)
+        
+        if(mag > self.magValue) {
+            print(mag)
+            self.backQueue.addOperation({() -> Void in self.motionEventOccurred()})
+        }
+    }
+    
+    func startCMMonitoring(){
+        if self.cmMotionManager.isDeviceMotionAvailable {
+            // update from this queue
+            self.cmMotionManager.deviceMotionUpdateInterval = UPDATE_INTERVAL
+            self.cmMotionManager.startDeviceMotionUpdates(to: backQueue, withHandler:self.handleMotion)
+        }
+    }
+    
+    func motionEventOccurred() {
+        let data = self.ringBuffer.getDataAsVector() as NSArray
+        if data[0] as! Double == 0.0 {
+            print("not full full")
+        } else {
+            
+            //get the FFT of both buffers and add them up for prediction data
+            self.getPredictionData(data: (self.ringBuffer.getDataAsVector()+self.orientationBuffer.getDataAsVector()) as NSArray )
+            
+        }
+    }
+    
+    func postFeatureHandler(data:Data?, urlResponse:URLResponse?, error:Error?) -> Void{
+        if(!(error != nil)){
+            print(urlResponse!)
+            if let responseData = try? JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! Dictionary<String, Any> {
+                print(responseData)
+                
+                if let prediction = responseData["prediction"] as? String {
+                    var instrument="Hi-Hat"
+                    switch prediction {
+                    case "['Play Hi-Hat']":
+                        instrument="Hi-Hat"
+                    case "['Play Snare']":
+                        instrument="Snare"
+                    case "['Play Bass']":
+                        instrument="Bass"
+                    case "['Play Toms']":
+                        instrument="Toms"
+                    case "['Play Cymbal']":
+                        instrument="Cymbal"
+                    default:
+                        print("UNKNOWN")
+                    }
+                    
+                    let date = self.players[instrument]!["time"] as! Date
+                    let now = Date()
+                    let seconds = now.timeIntervalSince(date)
+                    print("Seconds: ",seconds)
+                    
+                    if seconds > TIME_DELAY {
+                        self.players[instrument]!["time"] = now
+                        self.players[instrument]!["index"] = ((self.players[instrument]!["index"] as! Int)+1)%3
+                    }
+                    
+                    (self.players[instrument]!["players"] as! Array<AVAudioPlayer>)[self.players[instrument]!["index"] as! Int].play()
+                    
+                    
+                }
+                else {
+                    print("could not convert prediction to string")
+                }
+            }
+            else {
+                print("Could not convert to dict")
+            }
+            
+        } else{
+            print(error!)
+        }
+    }
+    
+    
+    func getPredictionData(data:NSArray) {
+        let baseUrl = "\(SERVER_URL)/PredictOne"
+        let postUrl = NSURL(string: baseUrl)
+        self.numDataPoints = self.numDataPoints + 1
+        
+        
+        let jsonUpload:NSDictionary = ["feature": data, "dsid": DSID]
+        
+        let requestBody = try! JSONSerialization.data(withJSONObject: jsonUpload, options: JSONSerialization.WritingOptions.prettyPrinted)
+        var request = URLRequest(url: postUrl as! URL)
+        request.httpBody = requestBody
+        request.httpMethod = "POST"
+        
+        
+        let postTask = self.session.dataTask(with: request, completionHandler: postFeatureHandler)
+        
+        
+        
+        postTask.resume()
+        print(self.numDataPoints)
+    }
+
 
     // MARK: - Table view data source
 
@@ -151,99 +267,5 @@ class PlayTableViewController: UITableViewController, URLSessionTaskDelegate {
         // Pass the selected object to the new view controller.
     }
     */
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        self.cmMotionManager.stopDeviceMotionUpdates()
-    }
-    
-    
-    func handleMotion(motion:CMDeviceMotion?, error:Error?)->Void{
-        self.ringBuffer.addNewData(Float((motion?.userAcceleration.x)!), withY: Float((motion?.userAcceleration.y)!), withZ: Float((motion?.userAcceleration.z)!))
-        
-        self.orientationBuffer.addNewData(Float((motion?.attitude.pitch)!), withY: Float((motion?.attitude.roll)!), withZ: Float((motion?.attitude.yaw)!))
-        
-        let mag = fabs((motion?.userAcceleration.x)!)+fabs((motion?.userAcceleration.y)!)+fabs((motion?.userAcceleration.z)!)
-        
-        if(mag > self.magValue) {
-            print(mag)
-            self.backQueue.addOperation({() -> Void in self.motionEventOccurred()})
-        }
-    }
-    
-    func startCMMonitoring(){
-        if self.cmMotionManager.isDeviceMotionAvailable {
-            // update from this queue
-            self.cmMotionManager.deviceMotionUpdateInterval = UPDATE_INTERVAL
-            self.cmMotionManager.startDeviceMotionUpdates(to: backQueue, withHandler:self.handleMotion)
-        }
-    }
-    
-    func motionEventOccurred() {
-        let data = self.ringBuffer.getDataAsVector() as NSArray
-        if data[0] as! Double == 0.0 {
-            print("not full full")
-        } else {
-            
-            //get the FFT of both buffers and add them up for prediction data
-            self.getPredictionData(data: (self.ringBuffer.getDataAsVector()+self.orientationBuffer.getDataAsVector()) as NSArray )
-            
-        }
-    }
-    
-    func postFeatureHandler(data:Data?, urlResponse:URLResponse?, error:Error?) -> Void{
-        if(!(error != nil)){
-            print(urlResponse!)
-            if let responseData = try? JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! Dictionary<String, Any> {
-                print(responseData)
-                
-                if let prediction = responseData["prediction"] as? String {
-                    switch prediction {
-                    case "['Play Hi-Hat']":
-                        self.hihatPlayer.play()
-                    case "['Play Snare']":
-                        self.snarePlayer.play()
-                    case "['Play Kick Drum']":
-                        self.kickdrumPlayer.play()
-                    default:
-                        print("UNKNOWN")
-                    }
-                }
-                else {
-                    print("could not convert prediction to string")
-                }
-            }
-            else {
-                print("Could not convert to dict")
-            }
-            
-        } else{
-            print(error!)
-        }
-        
-    }
-    
-    func getPredictionData(data:NSArray) {
-        let baseUrl = "\(SERVER_URL)/PredictOne"
-        let postUrl = NSURL(string: baseUrl)
-        self.numDataPoints = self.numDataPoints + 1
-        
-        
-        let jsonUpload:NSDictionary = ["feature": data, "dsid": DSID]
-        
-        let requestBody = try! JSONSerialization.data(withJSONObject: jsonUpload, options: JSONSerialization.WritingOptions.prettyPrinted)
-        var request = URLRequest(url: postUrl as! URL)
-        request.httpBody = requestBody
-        request.httpMethod = "POST"
-        
-        
-        let postTask = self.session.dataTask(with: request, completionHandler: postFeatureHandler)
-        
-        
-        
-        postTask.resume()
-        print(self.numDataPoints)
-    }
-
 
 }
