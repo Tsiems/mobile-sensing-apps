ccd3cb5f8d9558f6e2b28124360a993706c0961d
DNguyen17 <danhn@smu.edu>
2016-12-08 12:03:48 -0600

    can delete recording

--- AirDrummer/AirDrummer/RecordingsTableViewController.swift
+++ AirDrummer/AirDrummer/RecordingsTableViewController.swift
@@ -86,7 +86,6 @@ class RecordingsTableViewController: UITableViewController, AVAudioPlayerDelegat
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(recordings[indexPath.row])
         let recordingURL = directoryContents.filter{ $0.deletingPathExtension().lastPathComponent == recordings[indexPath.row] }[0]
         audioPlayer = try! AVAudioPlayer(contentsOf: recordingURL)
         audioPlayer.prepareToPlay()
@@ -101,17 +100,21 @@ class RecordingsTableViewController: UITableViewController, AVAudioPlayerDelegat
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            let recordingURL = directoryContents.filter{ $0.deletingPathExtension().lastPathComponent == recordings[indexPath.row] }[0]
+            recordings.remove(at: indexPath.row)
+            try! FileManager.default.removeItem(at: recordingURL)
+            
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
