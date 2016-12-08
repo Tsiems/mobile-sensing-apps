0098dfec926520f4ec2d8e9e0bf3a78cef3f1bd2
egabrielsen <egabrielsen@smu.edu>
2016-12-08 11:38:43 -0600

    Stuff

---
+++ AirDrummer/AirDrummer/DrumKitTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  DrumKitTableViewCell.swift
+//  AirDrummer
+//
+//  Created by Erik Gabrielsen on 12/8/16.
+//  Copyright © 2016 Danh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class DrumKitTableViewCell: UITableViewCell {
+    @IBOutlet weak var kitLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
