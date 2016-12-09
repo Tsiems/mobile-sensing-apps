d52ba7406e754512c6441a7a245a5c94eec4ea77
egabrielsen <egabrielsen@smu.edu>
2016-12-09 10:40:36 -0600

    changed segue

--- AirDrummer/DrumKit.swift
+++ AirDrummer/DrumKit.swift
@@ -32,23 +32,3 @@ class DrumKit: NSObject,NSCoding {
     }
 
 }
-
-import UIKit
-import QuartzCore
-
-class SegueFromLeft: UIStoryboardSegue {
-    
-    override func perform() {
-        let src: UIViewController = self.source
-        let dst: UIViewController = self.destination
-        let transition: CATransition = CATransition()
-        let timeFunc : CAMediaTimingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-        transition.duration = 0.25
-        transition.timingFunction = timeFunc
-        transition.type = kCATransitionPush
-        transition.subtype = kCATransitionFromLeft
-        src.navigationController!.view.layer.add(transition, forKey: kCATransition)
-        src.navigationController!.pushViewController(dst, animated: false)
-    }
-    
-}
